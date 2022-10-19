21e47bb7b2a5142cf2b1c247672ca165600ff0b1
Michael <mickeymurage@gmail.com>
2022-10-19 15:01:26 +0300

    initialized category, seller and product data

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ApplicationController
+	rescue_from ActiveRecord::RecordNotFound, with: :render_prod_not_found
+	def index
+		render json: Product.all, status: :ok
+	end
+
+	def show
+		prod = Product.find(params[:id])
+		render json: prod, status: :ok
+	end
+
+	private
+
+	def render_prod_not_found
+		render json: {error: "Product not found"}, status: :not_found
+	end
+end
