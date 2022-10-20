dc7bd2b578037dc82396267e4c10780e057942b3
Miriam Anyango <minaoanya@gmail.com>
2022-10-20 14:52:22 +0300

    Fetch products

--- client/src/components/Jewellery.jsx
+++ client/src/components/Jewellery.jsx
@@ -1,21 +1,56 @@
-import React from 'react'
-import '../styles/Jewellery.css'
-import { Card } from 'antd'
+import React, { useState, useEffect } from "react";
+import "../styles/Jewellery.css";
+import { Row, Col } from "antd";
+import { Card } from "antd";
+
+const { Meta } = Card;
 
 function Jewellery() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/products")
+    .then(res => res.json())
+    .then((data)=> setProducts(data))
+  },[])
+
+
   return (
     <div>
-        <div>
-            <h3 className='jewel'>Jewelery</h3>
-            <hr></hr>
-        </div>
-        <Card>
-          hoverable
-          style={{ width: 240}}
-          cover={<img alt='ceramics' src='https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png'/>}
-        </Card>
+      <div>
+        <h3 className="jewel">Jewellery</h3>
+        <hr id="myhr"></hr>
+      </div>
+      <div>
+      {products.map((product) => (
+         <div className="block-feature">
+         <div className="container-fluid">
+           <Row gutter={[32, 40]}>
+             <Col span={8}>
+               <Card
+                 hoverable
+                 style={{ width: 240 }}
+                 cover={
+                   <img
+                     alt="example"
+                     src={product.image}
+                   />
+                 }
+               >
+                 <Meta
+                   title="Europe Street beat"
+                   description="www.instagram.com"
+                 />
+               </Card>
+             </Col>
+           </Row>
+         </div>
+       </div>
+      ))}
+     </div>
     </div>
-  )
+  );
 }
 
-export default Jewellery
+export default Jewellery;
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_19_110131) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
