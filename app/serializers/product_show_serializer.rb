3a0758347ed762569eb5e2a61ea14b1ef046b923
Michael <mickeymurage@gmail.com>
2022-10-20 19:44:49 +0300

    optimised products index request to show bids also

---
+++ app/serializers/product_show_serializer.rb
@@ -0,0 +1,4 @@
+class ProductShowSerializer < ActiveModel::Serializer
+  attributes :id, :image_1, :image_2, :image_3, :name, :location, :date, :time, :starting_price, :seller_id, :category_id
+	belongs_to :bids
+end
