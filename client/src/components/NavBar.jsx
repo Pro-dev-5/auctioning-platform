e411b6e10f46b9f25368cea0823422416d047080
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-10-20 17:48:05 +0300

    Update: Header by adding logo and removes unnecessary navlinks

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -36,17 +36,6 @@ function NavBar() {
                     padding: '0 50px',
                 }}
                 >
-                <div className="site-layout-content">Content
-                </div>
-                <Breadcrumb
-                    style={{
-                    margin: '16px 0',
-                    }}
-                >
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>                  
-                </Breadcrumb>
                 <div className="site-layout-content">Content</div>
                 </Content>
                 <Footer
