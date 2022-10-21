f1e77f20f39d3172646a19505959cf66f9699273
maryanne-njeri <maryanjeryrose@gmail.com>
2022-10-21 07:50:27 +0300

    Fix: Content layout

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Layout } from 'antd'
 import '../App.less'
+import Home from './Home'
 import Item from './Item'
 import Jewellery from './Jewellery'
-import Home from './Home'
-// import Jewellery from './Jewellery'
-
-
 import BuyerLogin from './BuyerLoginForm'
 import BuyerSignup from './BuyerSignUpForm'
 import SellerLogin from './SellerLoginForm'
 import SellerSignup from './SellerSignUpForm'
 import NavBar from './NavBar'
+import Footer from './Footer'
+import '../styles/Nav.css'
+// import { Footer } from 'antd/lib/layout/layout'
 
 
 function App() {
@@ -35,17 +36,22 @@ function App() {
 
 
     <BrowserRouter>
-      <NavBar/>
-        <Routes>
-            <Route  path="/home" element={<Home />} />
-            <Route  path="/jewellery" element={<Jewellery />} />
-            <Route  path="/item" element={<Item/>} />
-            <Route  path="/buyerlogin" element={<BuyerLogin />} />
-            <Route  path="/buyersignup" element={<BuyerSignup />} />
-            <Route  path="/sellerlogin" element={<SellerLogin />} />
-            <Route  path="/sellersignup" element={<SellerSignup/>} />
-        </Routes>
-     </BrowserRouter>
+      <Layout className="layout"> {/* Element to revert if otherwise  */}
+        <NavBar/>
+        <div className='site-layout-content'>
+          <Routes>
+              <Route  path="/" element={<Home />} />
+              <Route  path="/jewellery" element={<Jewellery />} />
+              <Route  path="/item" element={<Item/>} />
+              <Route  path="/buyerlogin" element={<BuyerLogin />} />
+              <Route  path="/buyersignup" element={<BuyerSignup />} />
+              <Route  path="/sellerlogin" element={<SellerLogin />} />
+              <Route  path="/sellersignup" element={<SellerSignup/>} />
+          </Routes>
+        </div>
+          <Footer/>
+      </Layout>
+    </BrowserRouter>
 
     </div>
   )
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Col, Row } from 'antd'
 
 function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <div>
+                <div style={{ position: 'relative' }}>
+                    <h1 className="" style={{ paddingBottom: '20px' }}>Categories</h1>
+                    <div style={{ height: '4px', backgroundColor: '#ECC13B', width: '80px', position: 'absolute', top: '40px', left: '60px'}}></div>
+                    <Row>
+                        <Col span={8}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi corporis provident rem iure, commodi amet!</Col>
+                        <Col span={8}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi distinctio dolorem accusamus quod tenetur consequuntur.</Col>
+                        <Col span={8}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, sint? Distinctio doloremque architecto deleniti accusamus.</Col>
+                    </Row>
+                </div>
+            </div>
+        </div>
     )
 }
 
