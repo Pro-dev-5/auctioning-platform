99eee893e51950207164d8c51e6175031e402474
Calvin <calvinkioko91@gmail.com>
2022-10-24 10:13:37 +0300

    Edited styling for item

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,4 +1,4 @@
-import { Image, Button, Descriptions, PageHeader, Badge , Statistic, Tag, Tabs } from 'antd';
+import { Image, Button, Descriptions, PageHeader, Card , Statistic, Tag, Tabs } from 'antd';
 import React, { useState } from 'react';
 import '../styles/Item.css'
 const { TabPane } = Tabs;
@@ -6,12 +6,12 @@ const renderContent = (column = 2) => (
     <Descriptions size="small" column={column}>
         <Descriptions.Item label="Owner">Jane Doe</Descriptions.Item>
         <Descriptions.Item label="Association">
-        <a>421421</a>
+        <a>Item ID</a>
         </Descriptions.Item>
         <Descriptions.Item label="Start Time">2022-10-13</Descriptions.Item>
         <Descriptions.Item label="End Time">2022-10-14</Descriptions.Item>
         <Descriptions.Item label="Remarks">
-        Gonghu Road, Xihu District, Hangzhou, Zhejiang, China
+        In excellent condition
         </Descriptions.Item>
     </Descriptions>
 );
@@ -63,68 +63,50 @@ function Item() {
                     </PageHeader>
                 </div>
                 <div className='parent'>
-                <div className='child'>
-                    <Image
+                    <div className='child1'>
+                        <Image
+                            preview={{
+                            visible: false,
+                            }}
+                            width={200}
+                            src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"
+                            onClick={() => setVisible(true)}
+                        />
+                        <div
+                            style={{
+                            display: 'none',
+                            }}
+                        >
+                        <Image.PreviewGroup
                         preview={{
-                        visible: false,
+                            visible,
+                            onVisibleChange: (vis) => setVisible(vis),
                         }}
-                        width={200}
-                        src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp"
-                        onClick={() => setVisible(true)}
-                    />
-                    <div
-                        style={{
-                        display: 'none',
-                        }}
-                    >
-                    <Image.PreviewGroup
-                    preview={{
-                        visible,
-                        onVisibleChange: (vis) => setVisible(vis),
-                    }}
-                    >
-                    <Image src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp" />
-                    <Image src="https://gw.alipayobjects.com/zos/antfincdn/cV16ZqzMjW/photo-1473091540282-9b846e7965e3.webp" />
-                    <Image src="https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp" />
-                    </Image.PreviewGroup>
-                    </div>
+                        >
+                        <Image src="https://gw.alipayobjects.com/zos/antfincdn/LlvErxo8H9/photo-1503185912284-5271ff81b9a8.webp" />
+                        <Image src="https://gw.alipayobjects.com/zos/antfincdn/cV16ZqzMjW/photo-1473091540282-9b846e7965e3.webp" />
+                        <Image src="https://gw.alipayobjects.com/zos/antfincdn/x43I27A55%26/photo-1438109491414-7198515b166b.webp" />
+                        </Image.PreviewGroup>
+                        </div>
+                        <p>Click picture to view</p>
 
-                <div>
+                    <div>
 
                 </div>
                     
                 </div>
 
-                <div className=''>
-                <Descriptions title="User Info" layout="vertical" bordered>
-    <Descriptions.Item label="Product">Cloud Database</Descriptions.Item>
-    <Descriptions.Item label="Billing Mode">Prepaid</Descriptions.Item>
-    <Descriptions.Item label="Automatic Renewal">YES</Descriptions.Item>
-    <Descriptions.Item label="Order time">2018-04-24 18:00:00</Descriptions.Item>
-    <Descriptions.Item label="Usage Time" span={2}>
-      2019-04-24 18:00:00
-    </Descriptions.Item>
-    <Descriptions.Item label="Status" span={3}>
-      <Badge status="processing" text="Running" />
-    </Descriptions.Item>
-    <Descriptions.Item label="Negotiated Amount">$80.00</Descriptions.Item>
-    <Descriptions.Item label="Discount">$20.00</Descriptions.Item>
-    <Descriptions.Item label="Official Receipts">$60.00</Descriptions.Item>
-    <Descriptions.Item label="Config Info">
-      Data disk type: MongoDB
-      <br />
-      Database version: 3.4
-      <br />
-      Package: dds.mongo.mid
-      <br />
-      Storage space: 10 GB
-      <br />
-      Replication factor: 3
-      <br />
-      Region: East China 1<br />
-    </Descriptions.Item>
-  </Descriptions>
-);
+                <div className='child2'>
+                        <Card
+                            title="Item name"
+                            
+                            style={{
+                                width: 300,
+                            }}
+                            >
+                            <p>Item description</p>
+                        </Card>
+
                 </div>
                 </div>
         </div>
