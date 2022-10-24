1fb5c9e3cf3a058481ee7b9642222d7c10b962d3
Miriam Anyango <minaoanya@gmail.com>
2022-10-24 14:26:10 +0300

    Correct the Ceramics page

--- client/src/components/Ceramics.jsx
+++ client/src/components/Ceramics.jsx
@@ -3,20 +3,24 @@ import { Card } from "antd";
 import { useState } from "react";
 import { useEffect } from "react";
 import { toast, ToastContainer } from "react-toastify";
-import "../styles/ArtPage.css";
+import "../styles/Ceramics.css";
 import { Row, Col } from "antd";
 
 function Ceramics({ url }) {
   const [ceramics, setCeramics] = useState([]);
+  // const navigate = useNavigate()
+  // <button onClick={() => navigate('jewellery')}>View Products</button>
+
   useEffect(() => {
-    fetch(`${url}/products`).then((res) => {
-      console.log(res);
-      if (res.ok) {
-        res.json().then(setCeramics);
-      } else {
-        toast("Something went wrong with your request");
-      }
-    });
+    fetch(`${url}/products`)
+      .then((res) => {
+        if (res.ok) {
+          res.json().then(setCeramics);
+        } else {
+          toast("Something went wrong with your request");
+        }
+      })
+      .catch((err) => toast(err.message));
   }, []);
   return (
     <>
@@ -38,39 +42,36 @@ function Ceramics({ url }) {
 
 function CeramicsCard({ item }) {
   return (
-    <>
-      <hr className="myhr"></hr>
-      <div className="my-card">
-        <div className="container-fluid">
-          <Row gutter={[40, 40]}>
-            <Col span={8}>
-              <Card
-                hoverable
-                style={{ width: 300 }}
-                cover={
-                  <img
-                    alt=""
-                    src={
-                      !item.image_1
-                        ? "../../src/assets/artwork.jpeg"
-                        : item.image_1
-                    }
-                  />
-                }
-              >
-                <div className="cardcontent">
-                  <h4>{item.name}</h4>
-                  <p>Location: {item.location}</p>
-                  <p>Start Price: {item.starting_price}</p>
-                  <p>Time: {item.time}</p>
-                  <button>Place Bid</button>
-                </div>
-              </Card>
-            </Col>
-          </Row>
-        </div>
+    <div className="my-card">
+      <div className="container">
+        <Row gutter={[40, 40]}>
+          <Col span={8}>
+            <Card
+              hoverable
+              style={{ width: 300 }}
+              cover={
+                <img
+                  alt=""
+                  src={
+                    !item.image_1
+                      ? "../../src/assets/artwork.jpeg"
+                      : item.image_1
+                  }
+                />
+              }
+            >
+              <div className="cardcontent">
+                <h4>{item.name}</h4>
+                <p>Location: {item.location}</p>
+                <p>Start Price: {item.starting_price}</p>
+                <p>Time: {item.time}</p>
+                <button>Place Bid</button>
+              </div>
+            </Card>
+          </Col>
+        </Row>
       </div>
-    </>
+    </div>
   );
 }
 export default Ceramics;
