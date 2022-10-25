88c33963f03874bebe332f7ec8302765ec493934
Miriam Anyango <minaoanya@gmail.com>
2022-10-25 15:31:46 +0300

    Style the products pages and

--- client/src/components/Seller.jsx
+++ client/src/components/Seller.jsx
@@ -1,90 +1,135 @@
 import React, { useState, useEffect } from "react";
 
-function NewProduct() {
-    const [formData, setFormData] = useState({
-        image_1: "",
-        image_2: "",
-        image_3: "",
-        name: "",
-        location: "",
-        time: "",
-        date: "",
-        starting_price: 0,
-        category_id: 0
-    });
-  
+function Seller({url}) {
+  const [formData, setFormData] = useState({
+    image_1: "",
+    image_2: "",
+    image_3: "",
+    name: "",
+    location: "",
+    time: "",
+    date: "",
+    starting_price: 0,
+    category_id: 0,
+  });
 
- function handleChange(e){
-     setFormData({
-         ...formData,
-         [e.target.id]: e.target.value
-     });
- }
+  function handleSubmit(e){
+    e.preventDefault()
+    fetch(`${url}/products`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(r => r.json())
+    .then(data => {
+        console.log(data)
+    })
+
+    setFormData({
+    image_1: "",
+    image_2: "",
+    image_3: "",
+    name: "",
+    location: "",
+    time: "",
+    date: "",
+    starting_price: "",
+    category_id: ""
+    })
+}
+
+function handleChange(e){
+    setFormData({
+        ...formData, [e.target.name]: e.target.value,
+    });
+}  
 
   return (
     <>
       <div>Add New Product</div>
+
       <form>
-        <label htmlFor="image_1">Image 1:</label>
+        <label>Image 1:</label>
         <input
-          type="url"
-          id="image_1"
+          type="text"
+          name="image_1"
           value={formData.image_1}
           onChange={handleChange}
         />
-        <label htmlFor="image_2">Image 2:</label>
+
+        <label>Image 2:</label>
         <input
-          type="url"
-          id="image_2"
+          type="text"
+          name="image_2"
           value={formData.image_2}
           onChange={handleChange}
         />
-        <label htmlFor="image_3">Image 3:</label>
+
+        <label>Image 3:</label>
         <input
-          type="url"
-          id="image_3"
+          type="text"
+          name="image_3"
           value={formData.image_3}
           onChange={handleChange}
         />
-        <label htmlFor="name">Name:</label>
+
+        <label>Name:</label>
         <input
           type="text"
-          id="name"
+          name="name"
           value={formData.name}
           onChange={handleChange}
         />
-        <label htmlFor="location">Location:</label>
+
+        <label>Location:</label>
         <input
           type="text"
-          id="location"
+          name="location"
           value={formData.location}
           onChange={handleChange}
         />
-        <label htmlFor="time">Time:</label>
+
+        <label>Time:</label>
         <input
           type="time"
-          id="time"
+          name="time"
           value={formData.time}
           onChange={handleChange}
         />
-        <label htmlFor="date">Date:</label>
+
+        <label>Date:</label>
         <input
           type="date"
-          id="date"
+          name="date"
           value={formData.date}
           onChange={handleChange}
         />
-        <label htmlFor="starting_price">Starting Price:</label>
+
+        <label>Starting Price:</label>
         <input
           type="number"
-          id="starting_price"
+          name="starting_price"
           value={formData.starting_price}
           onChange={handleChange}
         />
-        <button type="submit">Add Product</button>
+
+        <input type="radio" id="art" name="category" value="art" />
+        <label>Art</label>
+        <br />
+
+        <input type="radio" id="ceramics" name="category" value="ceramics" />
+        <label>Ceramics</label>
+        <br />
+
+        <input type="radio" id="jewellery" name="category" value="jewellery" />
+        <label>Jewellery</label>
+
+        <button type='submit' onClick={handleSubmit}>Add Product</button>
       </form>
     </>
   );
 }
 
-export default NewProduct;
+export default Seller;
