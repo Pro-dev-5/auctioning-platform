b5b039d7154722300d5ed65bac54511680c0eaff
Michael <mickeymurage@gmail.com>
2022-10-28 12:53:23 +0300

    reconciled most components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,10 +4,10 @@ import '../App.less'
 import Home from './Home'
 import Item from './Item'
 import Jewellery from './Jewellery'
-import BuyerLogin from './BuyerLoginForm'
-import BuyerSignup from './BuyerSignUpForm'
-import SellerLogin from './SellerLoginForm'
-import SellerSignup from './SellerSignUpForm'
+// import BuyerLogin from './BuyerLoginForm'
+// import BuyerSignup from './BuyerSignUpForm'
+// import SellerLogin from './SellerLoginForm'
+// import SellerSignup from './SellerSignUpForm'
 import NavBar from './NavBar'
 import Footer from './Footer'
 import '../styles/Nav.css'
@@ -17,6 +17,8 @@ import SellerHome from './SellerHome'
 import Seller from './Seller'
 import { useEffect, useState } from 'react'
 import { toast, ToastContainer } from 'react-toastify'
+import SignUp from './Signup'
+import Login from './Login'
 // import { Footer } from 'antd/lib/layout/layout'
 
 function App() {
@@ -39,17 +41,17 @@ function App() {
         <NavBar/>
         <div className='site-layout-content' style={{ position: 'absolute', top: '80%', right: '0', left: '0' }}>
           <Routes>
-              <Route  path="/" element={<Home url={url}/>} />
-              <Route  path="/jewellery" element={<Jewellery url={url} />} />
-              <Route  path="/item/:id" element={<Item url={url}/>} />
-              <Route  path="/ceramics" element={<Ceramics url={url}/>} />
-              <Route  path="/seller" element={<Seller url={url} seller={seller}/>} />
-              <Route  path="/buyerlogin" element={<BuyerLogin url={url}/>} />
-              <Route  path="/buyersignup" element={<BuyerSignup url={url}/>} />
-              <Route  path="/sellerlogin" element={<SellerLogin url={url} setSeller={setSeller}/>}/>
-              <Route  path="/sellersignup" element={<SellerSignup url={url} setSeller={setSeller}/>} />
-              <Route  path="/art" element={<Art url={url}/>} />
-              <Route  path="/sellerhome" element={<SellerHome url={url}/>} />
+              <Route  path="/" element={<Home />} />
+              <Route  path="/jewellery" element={<Jewellery  />} />
+              <Route  path="/item/:id" element={<Item />} />
+              <Route  path="/ceramics" element={<Ceramics />} />
+              <Route  path="/seller" element={<Seller  seller={seller}/>} />
+              <Route  path="/login" element={<Login setSeller={setSeller}/>} />
+              <Route  path="/signup" element={<SignUp setSeller={setSeller}/>} />
+              {/* <Route  path="/sellerlogin" element={<SellerLogin  setSeller={setSeller}/>}/> */}
+              {/* <Route  path="/sellersignup" element={<SellerSignup  setSeller={setSeller}/>} /> */}
+              <Route  path="/art" element={<Art />} />
+              <Route  path="/sellerhome" element={<SellerHome />} />
           </Routes>
           <div style={{ marginTop: '100px' }}>
 
