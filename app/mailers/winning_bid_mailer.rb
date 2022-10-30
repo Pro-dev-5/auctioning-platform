8539764ed8b9baa51db448db5b9b7156e85ef690
Michael <mickeymurage@gmail.com>
2022-10-30 08:19:16 +0300

    tried action mailers

---
+++ app/mailers/winning_bid_mailer.rb
@@ -0,0 +1,15 @@
+class WinningBidMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.winning_bid_mailer.winner.subject
+  #
+  def winner(bid, person)
+    @bid = bid
+		@person = person
+
+    mail to: @person.email,
+					subject: "Winning bid"
+  end
+end
