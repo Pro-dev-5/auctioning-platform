8e5914a267c6e1efb4c8076d7c33296fd7a5af4e
Michael <mickeymurage@gmail.com>
2022-11-02 07:54:23 +0300

    forgot to add description to product serializer and controller

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :image_1, :image_2, :image_3, :name, :location, :date, :time, :starting_price, :user_id, :category_id, :current_bid
+  attributes :id, :image_1, :image_2, :image_3, :name, :location, :date, :time, :starting_price, :user_id, :category_id, :current_bid, :description
 end
