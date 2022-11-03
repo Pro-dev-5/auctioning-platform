32222a66ad23631c77bcf895510e0c9dd6caa4d4
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-11-03 13:38:21 +0300

    accepted merge

--- client/src/components/BidsDataDisplay.jsx
+++ client/src/components/BidsDataDisplay.jsx
@@ -1,67 +1,9 @@
 import React from 'react'
 import { Card } from 'antd';
 
+
 function BidsDataDisplay() {
-
-    const navigate = useNavigate();
-	const [product, setProduct] = useState([])
-  const [formData, setFormData] = useState({
-    productName: "",
-    latestBidPrice: "",
-    currentUser: "",
-    category_id: "",
-  });
-
-  useEffect(()=>{
-    fetch(`/api/categories`)
-    .then(res=>res.json())
-    .then(data => setProduct(data))
-
-    .catch(err=>toast(err.message))
-  }, [])
-
-  function handleSubmit(e){
-    e.preventDefault()
-		try {
-			fetch(`/api/products`,{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-					productName: formData.productName,
-					latestBidPrice: formData.latestBidPrice,
-					currentUser: formData.currentUser,
-					category_id: formData.category_id,
-				})
-    })
-    .then(r =>{
-			if(r.ok){
-				r.json().then(()=>{
-					toast("Bid placed successiful")
-					// navigate('/')
-				})
-			}
-		})
-		} catch (error) {
-			console.log(error.message)
-		}
-
-    setFormData({
-    productName: "",
-    latestBidPrice: "",
-    currentUser: "",
-    category_id: "",
     
-    })
-	}
-	
-	function handleChange(e){
-    setFormData({
-        ...formData, [e.target.name]: e.target.value,
-    });
-	}  
-
     return (
         <div className='block aboutBlock' style={{ marginTop: "80px", height: "65vh"}}>
             <div className="site-card-border-less-wrapper container-fluid">
@@ -72,10 +14,9 @@ function BidsDataDisplay() {
                     width: 300,
                 }}
                 >
-                <p>Product Name</p>
-                <p>Latest bid</p>
-                <p>Current User</p>
-                <p>Category</p>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
                 </Card>
             </div>
         </div>
