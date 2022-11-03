a0141489e91197cb71f4ba4399a019fb10f8f60f
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-11-03 12:27:01 +0300

    created bids data display

--- client/src/components/BidsDataDisplay.jsx
+++ client/src/components/BidsDataDisplay.jsx
@@ -2,6 +2,66 @@ import React from 'react'
 import { Card } from 'antd';
 
 function BidsDataDisplay() {
+
+    const navigate = useNavigate();
+	const [product, setProduct] = useState([])
+  const [formData, setFormData] = useState({
+    productName: "",
+    latestBidPrice: "",
+    currentUser: "",
+    category_id: "",
+  });
+
+  useEffect(()=>{
+    fetch(`/api/categories`)
+    .then(res=>res.json())
+    .then(data => setProduct(data))
+
+    .catch(err=>toast(err.message))
+  }, [])
+
+  function handleSubmit(e){
+    e.preventDefault()
+		try {
+			fetch(`/api/products`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+					productName: formData.productName,
+					latestBidPrice: formData.latestBidPrice,
+					currentUser: formData.currentUser,
+					category_id: formData.category_id,
+				})
+    })
+    .then(r =>{
+			if(r.ok){
+				r.json().then(()=>{
+					toast("Bid placed successiful")
+					// navigate('/')
+				})
+			}
+		})
+		} catch (error) {
+			console.log(error.message)
+		}
+
+    setFormData({
+    productName: "",
+    latestBidPrice: "",
+    currentUser: "",
+    category_id: "",
+    
+    })
+	}
+	
+	function handleChange(e){
+    setFormData({
+        ...formData, [e.target.name]: e.target.value,
+    });
+	}  
+
     return (
         <div className='block aboutBlock' style={{ marginTop: "80px", height: "65vh"}}>
             <div className="site-card-border-less-wrapper container-fluid">
@@ -12,9 +72,10 @@ function BidsDataDisplay() {
                     width: 300,
                 }}
                 >
-                <p>Card content</p>
-                <p>Card content</p>
-                <p>Card content</p>
+                <p>Product Name</p>
+                <p>Latest bid</p>
+                <p>Current User</p>
+                <p>Category</p>
                 </Card>
             </div>
         </div>
